560eec5154eee81ddc2d8e2248c67540caff1aab
Nguyễn Đặng Tuấn Kiệt <97293734+kietndt-3350@users.noreply.github.com>
2023-11-09 13:27:06 +0700

    Chapter 8: Static and Dynamic Rendering - 09/11/2023

--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -1,3 +1,68 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData
+} from '@/app/lib/data';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  // let revenue : any[] = [];
+  // let latestInvoices : any[] = [];
+  // let cardData: {
+  //   numberOfInvoices: number;
+  //   numberOfCustomers: number;
+  //   totalPaidInvoices: string;
+  //   totalPendingInvoices: string;
+  // } = {
+  //   numberOfInvoices: 0,
+  //   numberOfCustomers: 0,
+  //   totalPaidInvoices: '',
+  //   totalPendingInvoices: '',
+  // };
+
+  // await Promise.all([
+  //   fetchRevenue(),
+  //   fetchLatestInvoices(),
+  //   fetchCardData()
+  // ]).then((responses) => {
+  //   if (responses && responses.length === 3) {
+  //     revenue = responses[0];
+  //     latestInvoices = responses[1];
+  //     cardData = responses[2];
+  //   }
+  // }).catch((error) => {
+  //   console.error('Error fetching data:', error);
+  // });;
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          <Card title="Collected" value={totalPaidInvoices} type="collected" />
+          <Card title="Pending" value={totalPendingInvoices} type="pending" />
+          <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+          <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+          <RevenueChart revenue={revenue} />
+          <LatestInvoices latestInvoices={latestInvoices} />
+        </div>
+      </div>
+    </main>
+  );
 }
